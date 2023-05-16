410c69bcccc6278e0d076f800408e2ebdbf84c92
pratham0609 <77499319+pratham0609@users.noreply.github.com>
2023-05-16 10:09:28 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const path = require('path');
+const connectDB = require('./server/database/connection');
+const app = express();
+
+dotenv.config({path:'config.env'})
+const PORT = process.env.PORT || 8080
+
+//log requests 
+app.use(morgan('tiny'));
+
+//mongodb connection
+connectDB();
+
+//parse reqs
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.set("view engine","ejs");
+//app.set("views",path.resolve(__dirname,"views/ejs"));
+
+//load asets
+app.use('/css',express.static(path.resolve(__dirname,"assets/css")));
+app.use('/img',express.static(path.resolve(__dirname,"assets/img")));
+app.use('/js',express.static(path.resolve(__dirname,"assets/js")));
+//css/style.css
+
+//load router
+app.use('/',require('./server/routes/router'))
+
+
+app.listen(3000,()=>{console.log('Server is running on http://localhost:${8080}')});
